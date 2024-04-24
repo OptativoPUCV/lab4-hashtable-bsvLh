f2f4410ff312a446f92522b9bbdbf4e68f912548
bsvLh <163372911+bsvLh@users.noreply.github.com>
2024-04-24 23:34:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -90,7 +90,21 @@ void enlarge(HashMap *map)
 
 void eraseMap(HashMap *map, char *key) {}
 
-Pair *searchMap(HashMap *map, char *key) { return NULL; }
+Pair *searchMap(HashMap *map, char *key) 
+{  
+  if (map == NULL) return NULL;
+  long posicion = hash(key, map->capacity);
+  while (map->buckets[posicion] != NULL)
+    {
+      if (is_equal(map->buckets[posicion]->key, key))
+      {
+        map->current = posicion;
+        return map->buckets[posicion];
+      }
+      posicion = (posicion + 1) % map->capacity;
+    }
+  return NULL;
+}
 
 Pair *firstMap(HashMap *map) { return NULL; }
 
